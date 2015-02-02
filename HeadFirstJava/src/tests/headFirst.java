85900d4756d2eacf419ba55cd409dba9dd96fd68
Sergio <rurounisergii@hotmail.com>
2015-02-02 14:42:40 +0000

    Initial commit

---
+++ HeadFirstJava/src/tests/headFirst.java
@@ -0,0 +1,17 @@
+package tests;
+
+
+public class headFirst{
+	Integer i;
+	int j;
+	
+	public static void main(String[] args){
+		headFirst go = new headFirst();
+		go.launch();
+	}
+	public void launch(){
+		String s = 	String.format("%, d", 1000000000);
+		System.out.println(s);
+		
+	}
+}
